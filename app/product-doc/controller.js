1c6cfef5e31a37c2f1a0aed06564bbf6bec3499a
rouz majlessi <rmajlessi@gmail.com>
2017-02-01 19:34:04 -0800

    adjust accordion component

--- app/product-doc/controller.js
+++ app/product-doc/controller.js
@@ -8,7 +8,7 @@ export default Ember.Controller.extend({
   },
   items: Ember.computed(function() {
     return [
-      {title: "Item 1", details: "Details here"},
+      {title: "Actions:", details: "code-snippet"},
       {title: "Item 2", details: "Details here"},
       {title: "Item 3", details: "Details here"}
     ];
